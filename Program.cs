5a9a92da210e44cc9b5c26e687dc767d559ba9a0
sarah sida <nestsida@gmail.com>
2021-10-04 08:56:06 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Syntax_Sugar
+{
+    class Program
+    {
+        static void Main()
+        {
+            
+
+            int answer = 4;
+            string response;
+
+            if (answer < 9)
+            {
+                response = answer + " is less than nine";
+            }
+            else
+            {
+                response = answer + "greater than or equal to nine";
+            }
+            Console.WriteLine(response);
+
+
+            //String interpolation
+            if (int.Parse(response) <= 9) { Console.WriteLine("is less than nine"); }
+            if (int.Parse(response) >= 9) { Console.WriteLine("greater than or equal to nine"); }
+
+
+
+            //// inferred typing
+            //var answer = 4;
+            //var response = string();
+
+            //if (answer < 9)
+            //{
+            //    response = string(answer)+ "is less than nine";
+            //}
+            //else
+            //{
+            //    response = string(answer) + "greater than or equal to nine";
+            //}
+
+            //Console.WriteLine();
+
+        }
+     
+          
+    }
+}
